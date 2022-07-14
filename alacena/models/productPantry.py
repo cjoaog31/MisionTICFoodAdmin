f80d30fb43211e2ffaae8dbee4b6fb7f039491a3
cjoaog31 <profucarlo@gmail.com>
2022-07-14 18:26:34 -0500

    Se adiciona un campo faltante en el modelo de productPantry

--- alacena/models/productPantry.py
+++ alacena/models/productPantry.py
@@ -20,6 +20,7 @@ class ProductPantry(models.Model):
     added_by = models.ForeignKey(User,verbose_name='Creador', on_delete= models.DO_NOTHING, null= True)
     last_update_date = models.DateTimeField('Ultima modificacion', auto_now= True)
     first_update_date = models.DateTimeField('Agregago', auto_now_add= True)
+    expiration_date = models.DateField('Fecha de vencimiento')
     quantity = models.IntegerField('Cantidad')
     unit = models.CharField('Unidad', choices= Unit.choices, default= Unit.U, max_length=20)
     
