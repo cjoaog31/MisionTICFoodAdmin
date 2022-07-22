6443eba233b55af1abc1b20b50eea1e398c94db6
cjoaog31 <profucarlo@gmail.com>
2022-07-21 21:28:21 -0500

    Debug=False

--- FoodMonitor/settings.py
+++ FoodMonitor/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = environ.get('djangoSecret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
