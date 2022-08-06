4427bba723d99257cc8810c5520b5eeb939b8673
Carlostal <carlosandrestalerom@gmail.com>
2022-08-06 00:39:59 -0500

    Creacion de la vista inventario

--- alacena/views/vistasvue/inventario.vue
+++ alacena/views/vistasvue/inventario.vue
@@ -1,256 +1,210 @@
-
-
-
 <template>
-        <div style="margin: auto;" class="containerup">
-            
-             <div class="date">
-                <p class="text" style="font-size: 2.5vw; margin-top: 4%; margin-bottom: -15%;">JUNE</p>
-                <p class="text" style="font-size: 2.5vw; margin-bottom: -15%;">23rd</p>
-                <p class="text" style="font-size: 2.5vw;">00:00:00</p>
-             </div>
-
-             <div class="next">
-                <p class="text" style="font-size: 1.5vw; margin-top: 4%; margin-bottom: 0%;">To prepare next:</p>
-                <p class="text" style="font-size: 1.5vw; margin-bottom: -4%;">Cabbage - Bought on June 10th.</p>
-                <p class="text" style="font-size: 1.5vw; margin-bottom: -4%;">Carrot - Bought on June 12th.</p>
-                <p class="text" style="font-size: 1.5vw; margin-bottom: -4%;">Greek Yogurt - Expires on June 25th.</p>
-             </div>
-
-            <div class="recipe">
-
-            </div>
-            </div>
-        <div style="margin: auto;" class="containerdown">
-
-            <form action="inventory.html" class="inventory">
-            <button class="inventory-text">
-            INVENTARIO
-            </button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesinventory.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100, 10000, 25000" rel="stylesheet">
+    <title>FoodMonitor Inventory</title>
+</head>
+<body>
+    <div class="box">
+        <div class="firstline">
+            <form action="mainscreen.html" class="formback">
+                <button type="submit" class="back">Back</button>
             </form>
 
-            
+            <img src="pictures/foodwhite.png" class="image"/>
 
-            <span class="verticalwish">
-
-                <form action="" class="wish">
-                    <button class="wish-text">
-                    WISHLIST
-                    </button>
-                </form>
-
-                <form action="" class="shop">
-                    <button class="shop-text">
-                    SHOPPING
-                    </button>
-                </form>
-
-            </span>
-
-            <form action="" class="users">
-                <button class="users-text">
-                USERS
-                </button>
-            </form>            
-
+            <form class="formuser">
+                <button type="submit" class="user">Username</button>
+            </form>
         </div>
+
+        <div class="secondline">
+            <h2 class="text">INVENTORY</h2>
+        </div>
+
+        <div class="thirdline">
+            <form action="" class="forminput">
+                <label for="search" class="label">Search</label>
+                <input type="text" id="search" class="input" placeholder="Search for products">
+            </form>
+            <form action="" class="formadd">
+                <button class="add">Add Product</button>
+            </form>
+        </div>
+    </div>
+
+    <table id="products" class="productos">
+        <tr> 
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Compra</th>
+            <th>Expiración</th>
+        </tr>
+
+        <!-- agregar mas table rows automaticamente-->
+    </table>
+</body>
+</html>
 </template>
 
 <script>
 
-
 </script>
 
 <style>
-.image{
-    margin-bottom: 0px;
-    width: 11.11vh;
-    height: 11.11vh;
-    margin-right: 40vw;
-    margin-left: 40vw;
+.box{
+    width: 100%;
+    height: 25vh;
+    background-image: linear-gradient(75deg, #7654E8, #2B06AC);
+    border-radius: 0% 0% 2vw 2vw;
 }
 
-.containerup{
+.firstline{
     display: flex;
+    flex-direction: row;
+    justify-content:space-evenly;
     align-items:center;
-    justify-content: space-evenly;
-    padding: 10px;
-    max-width: 85%;
+    height: 8vh;
 }
 
-.date {
-height: 22.91vh; width: 17.91vw; background-image: linear-gradient(75deg, aqua, teal);
-margin-right: 20px;
-border-radius: 35px;
-border-color: transparent;
-text-align:left;
-vertical-align: middle;
+.formback{
+    width: 10%;
+    height: 40%;
+    border: transparent;
+    border-radius: 1vw; 
+}
+
+.back{
+    width: 100%;
+    height: 100%;
+    border: transparent;
+    border-radius: 1vw;
+    color: #2B06AC;
+    font-weight: bolder;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.5vw;
+}
+
+.back:hover{
+    transform: scale(1.1);
+    transition: transform .2s;
+}
+
+.image{
+    width: 7vw;
+    height: 7vw;
+    margin-left: 35%;
+    margin-right: 35%;
+}
+
+.formuser{
+    width: 10%;
+    height: 40%;
+    border: transparent;
+    border-radius: 1vw; 
+}
+
+.user{
+    width: 100%;
+    height: 100%;
+    border: transparent;
+    border-radius: 1vw;
+    color: #2B06AC;
+    font-weight: bolder;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.5vw;
+}
+
+.user:hover{
+    transform: scale(1.1);
+    transition: transform .2s;
+}
+
+.secondline{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    height: 8vh;
+    margin-top: 1%;;
+    margin-bottom: 2%;;
 }
 
 .text{
-    margin-left: 11%;
-    font-size: auto; min-width: 10vw;
+    font-size: 4vw;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bolder;
+    color: white;
+}
+
+.thirdline{
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items:baseline;
+    vertical-align: bottom; 
+    height: 4vh;
+}
+
+.forminput{
+    width: 25%;
+    height: 100%;
+    margin-left: 2%;
+}
+
+.input{
+    width: 75%;
+    height: 50%;
+    border-radius: 1vw;
+    border: transparent;
+    text-align: center;
+}
+
+.label{
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.2vw;
+    font-weight: bolder;
+    color: white;
+}
+
+.formadd{
+    width: 10%;
+    height: 60%;
+    border-radius: 1vw;
+    border: transparent;
+    background-color: transparent;
+}
+
+.add{
+    width: 100%;
+    height: 100%;
+    border-radius: 1vw;
+    border: transparent;
+    background-color: transparent;
+    font-size: 1.2vw;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bolder;
+    color: white;
+}
+
+.productos{
+    width: 100%;
     font-family: 'Montserrat', sans-serif;
     color: white;
     font-weight: bolder;
 }
 
-.next {
-height: 22.91vh; width: 43.05vw; background-image: linear-gradient(75deg, aqua, teal);
-max-height: fit-content;
-border-radius: 35px;
-border-color: transparent;
-text-align: center;
-vertical-align: middle;
+#products{
+    padding-top: 1vw;
+    padding-bottom: 1vw;
+    text-align: center;
+    background-color: #2B06AC;
+    border-radius: 1.5vw;
 }
-
-.recipe {
-height: 22.91vh; width: 33.125vw; background-image: linear-gradient(75deg, aqua, teal);
-margin-left: 20px;
-border-radius: 35px;
-border-color: transparent;
-text-align: center;
-vertical-align: middle;
-}
-
-/* BOTTOM PART*/
-
-.verticalwish{
-    display: flex; flex-direction: column;
-    align-items: center;
-    min-width: 29.16%;
-}
-
-.containerdown{
-    display:flex;
-    align-items: center;
-    justify-content: space-evenly;
-    padding: 10px;
-}
-
-.inventory:hover{
-    transform: scale(1.05);
-    transition: transform .2s ;
-}
-
-.wish:hover{
-    transform: scale(1.05);
-    transition: transform .2s ;
-}
-
-.shop:hover{
-    transform: scale(1.05);
-    transition: transform .2s ;
-}
-
-.users:hover{
-    transform: scale(1.05);
-    transition: transform .2s ;
-}
-
-.inventory {
-    width: 29.16%; height: 38vh;
-    background-image: url(pictures/fruitdark.jpg);
-    background-size: cover; background-position: 61%;
-    margin-top: 35px ;
-    margin-bottom: 35px ;
-    margin-right: 20px ;
-    margin-left: 0px ;
-    border-radius: 0.8vw;
-    border-width: 0.8vw;
-    border-style: solid;
-    border-image: linear-gradient(75deg, aqua, teal)1;
-    box-shadow: 20px 10px 20px teal;
-}
-
-.inventory-text{
-    font-size: 2vw; 
-    color: white; 
-    font-family: 'Montserrat', sans-serif; 
-    font-weight: bolder ;
-    width: 100%; 
-    height: 38vh; 
-    background-color: transparent; 
-    border: transparent;
-}
-
-.users {
-    width: 29.16%; height: 38vh;
-    background-image: url(pictures/userdark.jpg);
-    background-size: cover; background-position: 50%;
-    margin-top: 35px ;
-    margin-bottom: 35px ;
-    margin-right: 0px ;
-    margin-left: 20px ;
-    border-radius: 0.8vw;
-    border-width: 0.8vw;
-    border-style: solid;
-    border-image: linear-gradient(75deg, aqua, teal)1;
-    box-shadow: 20px 10px 20px teal;
-}
-
-.users-text{
-    font-size: 2vw; 
-    color: white; 
-    font-family: 'Montserrat', sans-serif; 
-    font-weight: bolder ;
-    width: 100%; 
-    height: 38vh; 
-    background-color: transparent; 
-    border: transparent;
-    border-width: 30px;
-}
-
-.wish {
-    width: 100%; height: 15vh;
-    background-image: url(pictures/wishlistdark.jpg);
-    background-size: cover; background-position: 2% 2%;
-    margin-top: 0px ;
-    margin-bottom: 17px ;
-    margin-right: 0px ;
-    margin-left: 0px ;
-    border-radius: 0.8vw;
-    border-width: 0.8vw;
-    border-style: solid;
-    border-image: linear-gradient(75deg, aqua, teal)1;
-    box-shadow: 20px 10px 20px teal;
-}
-
-.wish-text{
-    font-size: 2vw; 
-    color: white; 
-    font-family: 'Montserrat', sans-serif; 
-    font-weight: bolder ;
-    width: 100%; 
-    height: 15vh; 
-    background-color: transparent; 
-    border: transparent;
-}
-
-.shop {
-    width: 100%; height: 15vh;
-    background-image: url(pictures/wishlistdark.jpg);
-    background-size: cover; background-position: 2% 2%;
-    margin-top: 17px ;
-    margin-bottom: 0px ;
-    margin-right: 0px ;
-    margin-left: 0px ;
-    border-radius: 0.8vw;
-    border-width: 0.8vw;
-    border-style: solid;
-    border-image: linear-gradient(75deg, aqua, teal)1;
-    box-shadow: 20px 10px 20px teal;
-}
-
-.shop-text{
-    font-size: 2vw; 
-    color: white; 
-    font-family: 'Montserrat', sans-serif; 
-    font-weight: bolder ;
-    width: 100%; 
-    height: 15vh; 
-    background-color: transparent; 
-    border: transparent;
-}
-
 </style>
